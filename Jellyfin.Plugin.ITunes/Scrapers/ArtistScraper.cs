bd50f88382795d2e860b2e525723e1484f1ff8cc
Dominik Krivohlavek <github@xkrivo.net>
2023-05-06 17:46:51 +0200

    Handle all artist images instead of first only

--- Jellyfin.Plugin.ITunes/Scrapers/ArtistScraper.cs
+++ Jellyfin.Plugin.ITunes/Scrapers/ArtistScraper.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading;
@@ -50,47 +49,20 @@ public class ArtistScraper : IScraper<ArtistScraper>
             return imageList;
         }
 
-        var artistResult = searchData.Results.FirstOrDefault();
-        if (artistResult is null)
+        foreach (var result in searchData.Results)
         {
-            _logger.LogError("No artists found even though reported count is {Count}", searchData.ResultCount);
-            return imageList;
+            if (result.ArtistLinkUrl is null)
+            {
+                _logger.LogDebug("Missing artist URL");
+                continue;
+            }
+
+            var image = await GetRemoteImage(result.ArtistLinkUrl).ConfigureAwait(false);
+            if (image is not null)
+            {
+                imageList.Add(image);
+            }
         }
-
-        if (artistResult.ArtistLinkUrl is null)
-        {
-            _logger.LogDebug("No artist link found");
-            return imageList;
-        }
-
-        _logger.LogDebug("Using {Url} to get artist images", artistResult.ArtistLinkUrl);
-        var page = await OpenPage(artistResult.ArtistLinkUrl).ConfigureAwait(false);
-        var imageUrl = page.Head.SelectSingleNode(ImageXPath)?.TextContent;
-        if (imageUrl is null)
-        {
-            _logger.LogError("No image URL found in {Url}", artistResult.ArtistLinkUrl);
-            return imageList;
-        }
-
-        _logger.LogDebug("Using {Url} to get image URLs", imageUrl);
-
-        // The artwork size can vary quite a bit, but for our uses, 1400x1400 should be plenty.
-        // https://artists.apple.com/support/88-artist-image-guidelines
-        var thumbImageUrl = imageUrl.Replace("1200x630cw", "100x100cc", StringComparison.OrdinalIgnoreCase);
-        var primaryImageUrl = imageUrl.Replace("1200x630cw", "1400x1400cc", StringComparison.OrdinalIgnoreCase);
-
-        _logger.LogDebug("Primary image URL: {Url}", primaryImageUrl);
-        _logger.LogDebug("Thumb image URL: {Url}", thumbImageUrl);
-
-        imageList.Add(new RemoteImageInfo
-        {
-            ProviderName = "Apple Music",
-            Url = primaryImageUrl,
-            Type = ImageType.Primary,
-            ThumbnailUrl = thumbImageUrl,
-            Height = 1400,
-            Width = 1400
-        });
 
         return imageList;
     }
@@ -108,5 +80,37 @@ public class ArtistScraper : IScraper<ArtistScraper>
     {
         var context = BrowsingContext.New(_config);
         return await context.OpenAsync(url).ConfigureAwait(false);
+    }
+
+    private async Task<RemoteImageInfo?> GetRemoteImage(string url)
+    {
+        _logger.LogDebug("Using {Url} to get artist images", url);
+        var page = await OpenPage(url).ConfigureAwait(false);
+        var imageUrl = page.Head.SelectSingleNode(ImageXPath)?.TextContent;
+        if (imageUrl is null)
+        {
+            _logger.LogError("No image URL found in page at {Url}", url);
+            return null;
+        }
+
+        _logger.LogDebug("Using {Url} to get image URLs", imageUrl);
+
+        // The artwork size can vary quite a bit, but for our uses, 1400x1400 should be plenty.
+        // https://artists.apple.com/support/88-artist-image-guidelines
+        var thumbImageUrl = imageUrl.Replace("1200x630cw", "100x100cc", StringComparison.OrdinalIgnoreCase);
+        var primaryImageUrl = imageUrl.Replace("1200x630cw", "1400x1400cc", StringComparison.OrdinalIgnoreCase);
+
+        _logger.LogDebug("Primary image URL: {Url}", primaryImageUrl);
+        _logger.LogDebug("Thumb image URL: {Url}", thumbImageUrl);
+
+        return new RemoteImageInfo
+        {
+            ProviderName = "Apple Music",
+            Url = primaryImageUrl,
+            Type = ImageType.Primary,
+            ThumbnailUrl = thumbImageUrl,
+            Height = 1400,
+            Width = 1400
+        };
     }
 }
