f09eaec64a142ceaa76bb1c4e01b6d4ab1c5296c
Dominik Krivohlavek <github@xkrivo.net>
2023-05-06 11:38:20 +0200

    Remove unused code

--- Jellyfin.Plugin.ITunes/Providers/ITunesArtistImageProvider.cs
+++ Jellyfin.Plugin.ITunes/Providers/ITunesArtistImageProvider.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using HtmlAgilityPack;
-using Jellyfin.Plugin.ITunes.Dtos;
 using Jellyfin.Plugin.ITunes.Scrapers;
 using MediaBrowser.Common.Net;
 using MediaBrowser.Controller.Entities;
@@ -84,55 +80,6 @@ public class ITunesArtistImageProvider : IRemoteImageProvider, IHasOrder
         var encodedName = Uri.EscapeDataString(artist.Name);
         var searchUrl = $"https://itunes.apple.com/search?term=${encodedName}&media=music&entity=musicArtist&attribute=artistTerm";
         return await _scraper.GetImages(searchUrl, cancellationToken).ConfigureAwait(false);
-    }
-
-    private async Task<IEnumerable<RemoteImageInfo>> GetImagesInternal(string url, CancellationToken cancellationToken)
-    {
-        List<RemoteImageInfo> list = new List<RemoteImageInfo>();
-
-        var iTunesAlbumDto = await _httpClientFactory
-            .CreateClient(NamedClient.Default)
-            .GetFromJsonAsync<ITunesArtistDto>(new Uri(url), cancellationToken)
-            .ConfigureAwait(false);
-
-        if (iTunesAlbumDto is not null && iTunesAlbumDto.ResultCount > 0)
-        {
-            var result = iTunesAlbumDto.Results.First();
-            if (result.ArtistLinkUrl is not null)
-            {
-                _logger.LogDebug("URL: {0}", result.ArtistLinkUrl);
-                HtmlWeb web = new HtmlWeb();
-                var doc = web.Load(new Uri(result.ArtistLinkUrl));
-                var navigator = (HtmlAgilityPack.HtmlNodeNavigator)doc.CreateNavigator();
-
-                var metaOgImage = navigator.SelectSingleNode("/html/head/meta[@property='og:image']/@content");
-
-                if (metaOgImage != null)
-                {
-                    _logger.LogDebug("Node: {0} | {1}", metaOgImage.NodeType, metaOgImage.Value);
-
-                    // The artwork size can vary quite a bit, but for our uses, 1400x1400 should be plenty.
-                    // https://artists.apple.com/support/88-artist-image-guidelines
-                    var image100 = metaOgImage.Value.Replace("1200x630cw", "100x100cc", StringComparison.OrdinalIgnoreCase);
-                    var image1400 = metaOgImage.Value.Replace("1200x630cw", "1400x1400cc", StringComparison.OrdinalIgnoreCase);
-
-                    list.Add(
-                        new RemoteImageInfo
-                        {
-                            ProviderName = Name,
-                            Url = image1400,
-                            Type = ImageType.Primary,
-                            ThumbnailUrl = image100
-                        });
-                }
-            }
-        }
-        else
-        {
-            return list;
-        }
-
-        return list;
     }
 
     /// <inheritdoc />
