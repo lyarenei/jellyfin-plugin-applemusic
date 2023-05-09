1c79f108018c40bab2439ead5384c26823b2f48f
Dominik Krivohlavek <github@xkrivo.net>
2023-05-09 23:00:03 +0200

    Revert "Set album artist provider ID for artist"
    This reverts commit d0aee3e0528622131d79e28f8de6095321637d48.

--- Jellyfin.Plugin.ITunes/Providers/ITunesArtistMetadataProvider.cs
+++ Jellyfin.Plugin.ITunes/Providers/ITunesArtistMetadataProvider.cs
@@ -75,7 +75,6 @@ public class ITunesArtistMetadataProvider : IRemoteMetadataProvider<MusicArtist,
         }
 
         metadataResult.Item.SetProviderId(ITunesProviderKey.Artist.ToString(), scrapedArtist.Id);
-        metadataResult.Item.SetProviderId(ITunesProviderKey.AlbumArtist.ToString(), scrapedArtist.Id);
         return metadataResult;
     }
 
