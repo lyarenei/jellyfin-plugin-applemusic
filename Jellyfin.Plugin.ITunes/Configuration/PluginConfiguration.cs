a34c24dd396a7bd67eb3ca53fbd063aab834ead8
Dominik Krivohlavek <github@xkrivo.net>
2024-08-23 10:19:54 +0200

    Add lang code option

--- Jellyfin.Plugin.ITunes/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.ITunes/Configuration/PluginConfiguration.cs
@@ -7,5 +7,26 @@ namespace Jellyfin.Plugin.ITunes.Configuration
     /// </summary>
     public class PluginConfiguration : BasePluginConfiguration
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+        /// </summary>
+        public PluginConfiguration()
+        {
+            LangCode = "us";
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+        /// </summary>
+        /// <param name="langCode">Language code.</param>
+        public PluginConfiguration(string langCode)
+        {
+            LangCode = langCode;
+        }
+
+        /// <summary>
+        /// Gets or sets a language code to use in URLs.
+        /// </summary>
+        public string LangCode { get; set; }
     }
 }
