3e57fca435df99dff11820186c8050120e66c9b3
Dominik Krivohlavek <github@xkrivo.net>
2024-10-14 21:11:47 +0200

    Trigger CI

--- Jellyfin.Plugin.ITunes/ITunesPlugin.cs
+++ Jellyfin.Plugin.ITunes/ITunesPlugin.cs
@@ -23,7 +23,7 @@ public class ITunesPlugin : BasePlugin<PluginConfiguration>
     }
 
     /// <inheritdoc />
-    public override string Name => "Apple Music";
+    public override string Name => "Apple Musicc";
 
     /// <inheritdoc />
     public override Guid Id => Guid.Parse("a9f62a44-fea5-46c3-ac26-55abba29c7c8");
