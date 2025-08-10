d13c03cb8abb86ca24be86807b67c2ddd36b307d
Dominik Krivohlavek <github@xkrivo.net>
2025-08-10 13:32:59 +0200

    Remove unused imports

--- Jellyfin.Plugin.AppleMusic/Utils/PluginUtils.cs
+++ Jellyfin.Plugin.AppleMusic/Utils/PluginUtils.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Globalization;
 using System.Linq;
-using Jellyfin.Plugin.AppleMusic.ExternalIds;
-using MediaBrowser.Model.Entities;
 
 namespace Jellyfin.Plugin.AppleMusic.Utils;
 
