f322b19b3cf83c31a7373651f9337dd8bb5d07fa
Dominik Krivohlavek <github@xkrivo.net>
2025-08-10 13:08:54 +0200

    Remove unused util

--- Jellyfin.Plugin.AppleMusic/Utils/PluginUtils.cs
+++ Jellyfin.Plugin.AppleMusic/Utils/PluginUtils.cs
@@ -45,31 +45,6 @@ public static class PluginUtils
     }
 
     /// <summary>
-    /// Get provider URL specified by <see cref="ProviderKey"/>.
-    /// </summary>
-    /// <param name="item">Item to get the provider URL for.</param>
-    /// <param name="key">Kind of provider URL to get.</param>
-    /// <returns>Item provider URL. Empty if not found.</returns>
-    public static string GetProviderUrl(IHasProviderIds item, ProviderKey key)
-    {
-        var providerId = item.GetProviderId(key.ToString());
-        if (providerId is null)
-        {
-            return string.Empty;
-        }
-
-        string? urlFormat = key switch
-        {
-            ProviderKey.ITunesAlbum => new ITunesAlbumExternalId().UrlFormatString,
-            ProviderKey.ITunesAlbumArtist => new ITunesAlbumArtistExternalId().UrlFormatString,
-            ProviderKey.ITunesArtist => new ITunesArtistExternalId().UrlFormatString,
-            _ => null
-        };
-
-        return urlFormat is not null ? string.Format(CultureInfo.InvariantCulture, urlFormat, providerId) : string.Empty;
-    }
-
-    /// <summary>
     /// Get Apple Music ID from Apple Music URL.
     /// The URL format is always "https://music.apple.com/us/[item type]/[ID]".
     /// </summary>
